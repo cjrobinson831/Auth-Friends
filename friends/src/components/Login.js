35c8d112dd1bb079b3005e01ceb8d040e97f6a9b
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-15 20:08:50 -0400

    login

---
+++ friends/src/components/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import axiosWithAuth from '../utils/axiosWithAuth';
+
+const Login = props => {
+    const [userCredentils, setuserCredentials] = useState({ username: '', password: '' })
+
+    const handleChange = e => {
+        setuserCredentials(
+            ...userCredentils,
+            [e.target.name]: e.target.value
+        )
+    }
+}
+
+const onSubmit = e => {
+    e.preventDefault()
+    axiosWithAuth()
+        .post('/login', userCredentils)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload)
+        })
+        .catch(err => console.log(err))
+}
+
+return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input type='text' name='username' value={userCredentils.username} onChange={handleChange} />
+            <input type='password' name='password' value={userCredentils} onChange={handleChange} />
+            <button> Log In </button>
+        </form>
+    </div>
+);
+
+export default Login;
